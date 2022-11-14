e55e39145e677789c60440edb76f0fabf1a25a1a
yuliussetyawan <yuliusetyawan@gmail.com>
2022-11-14 15:23:43 +0700

    Finishing touch (check required, length, email validation)

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username")
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
 
-
-const showError = (input, messages) =>{
-    const formControl = input.parentElement;
-    formControl.className = "form-control error";
-    const small = formControl.querySelector("small");
-    small.innerText = messages;
+const showError = (input, messages) => {
+  // target the form-control class
+  const formControl = input.parentElement
+  formControl.className = 'form-control error'
+  const small = formControl.querySelector('small')
+  small.innerText = messages
 }
 
-const showSucces = (input) =>{
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
+const showSucces = input => {
+  const formControl = input.parentElement
+  formControl.className = 'form-control success'
 }
 
-function validateEmail(email) {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(String(email).toLowerCase());
+const validateEmail = input => {
+  const re = /^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]([a-zA-Z]{2,9})$/
+  // return re.test(String(email).toLowerCase());
+  if (input.value.trim().match(re)) {
+    showSucces(input)
+  } else {
+    showError(input, `${getFieldName(input)} is not valid`)
   }
+}
 
-
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-
-    if (username.value === ""){
-        showError(username, "Username is required");
+const checkRequired = inputArr => {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`)
+    } else {
+      showSucces(input)
     }
-    else{
-        showSucces(username)
-    }
+  })
+}
 
-    if (email.value === ""){
-        showError(email, "Email is required");
-    }
-    else{
-        if (!validateEmail(email.value)){
-            showError(email, "Email is incorrect");
-        }
-        else{
-        showSucces(email)
-        }
+const checkPasswordMatch = (pass1, pass2) => {
+    if (pass1.value !== pass2.value){
+        showError(pass2, "The password is not match");
     }
 
-    if (password.value ===  ""){
-        showError(password, "Password is required");
-    }
-    else{
-        showSucces(password)
-    }
+}
 
-    if (password2.value === ""){
-        showError(password2, "Password 2 is required");
-    }
-    else{
-        showSucces(password2)
-    }
-});
+const checkLength = (input, min, max) => {
+  if (input.value.trim().length < min) {
+    showError(
+      input,
+      `${getFieldName(
+        input
+      )} must be at least ${min} and max ${max} characters length`
+    )
+  } else if (input.value.trim().length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters `
+    )
+  } else {
+    showSucces(input)
+  }
+}
 
+// capitalize the first letter
+const getFieldName = input => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  checkRequired([username, email, password, password2])
+  checkLength(username, 3, 15)
+  checkLength(password, 6, 25)
+  validateEmail(email)
+  checkPasswordMatch(password, password2)
+})
